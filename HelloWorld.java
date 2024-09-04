235aa232369d6cedef7b559399661065b348ba9c
lujiayi-qi <3411474576@qq.com>
2024-09-04 19:16:11 +0800

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-public static void main(String[] args) {
-System.out.println("Hello,Zhangyingyi!");
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
-}
+
